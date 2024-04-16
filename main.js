6a94e6e58f3ebad945c06d5cdf58e8b358168dc8
lsoliveiraa <129184876+lsoliveiraa@users.noreply.github.com>
2024-04-16 08:32:37 -0300

    ghost

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
-
+const texto = document.querySelector(".aba-conteudo");
 
 for(let i=0; i <  botoes.length; i++){
    botoes[i].onclick = function(){
@@ -10,6 +10,15 @@ for(let i=0; i <  botoes.length; i++){
     
 
     botoes[i].classList.add("ativo"); 
+    textos[i].classList.add("ativo");
    }
     console.log(botoes[i]);
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+let tempoAtual = new Date();
+ 
+contadores[0].textContent = tempoObjetivo1;
+
+contadores[0].textContent = "Contagem regressiva";
