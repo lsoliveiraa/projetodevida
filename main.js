f8bf2d0acc6ee04856d23b058241a194f99ebf45
lsoliveiraa <129184876+lsoliveiraa@users.noreply.github.com>
2024-05-02 09:24:58 -0300

    booya

--- main.js
+++ main.js
@@ -44,22 +44,31 @@ minutos %= 60;
 horas %= 24;
 
 if (tempoFinal > 0){
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    return [dias,horas,minutos,segundos];
 } else {
-    return "Prazo Finalizado";
+    return [0,0,0,0];
+}
+
+return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+
 }
 
 
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 
-}
-function atualizaCronometro();
+function atualizaCronometro(){ 
+    document.getElementById("dias0").textContent = calculaTempo(tempos[1]) [0];
+        document.getElementById("horas0").textContent = calculaTempo(tempos[1]) [1]
+        document.getElementById("min0").textContent = calculaTempo(tempos[1]) [2]
+        document.getElementById("seg0").textContent = calculaTempo(tempos[1]) [3]
     for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);   
+        //contadores[i].textContent = calculaTempo(tempos[i]);   
     }
+}
+
 }
 function comecaCronometro()
  atualizaCronometro();
 setInterval(atualizaCronometro,1000);
 
 comecaCronometro();
+
